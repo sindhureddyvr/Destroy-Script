26630622878c6b09a597fd37a08efd3c1e84900c
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-09 18:26:16 +0000

    Changed the colored text to normal

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-echo -e "\e[31m\e[1mLab4 Destroy script"
+echo -e "\e[1mLab5 Destroy script\e[0m"
 
-echo -e "\e[36mCollect instance id's attached to the auto-scaling-group"
+echo -e "Collect instance id's attached to the auto-scaling-group"
 instance_id=$(aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].[InstanceId]')
 
-echo -e "\e[95mSet desired capacity to 0"
+echo -e "\e[1mSet desired capacity to 0 \e[0m"
 aws autoscaling set-desired-capacity --auto-scaling-group-name webserverdemo --desired-capacity 0
+echo -e "Desired Capacity is set to 0"
 
-echo -e "\e[33mWait until the instances are terminated"
+echo -e "\e[1mWait until the instances are terminated\e[0m"
 aws ec2 wait instance-terminated --instance-ids $instance_id
-echo -e "\e[93mWait is completed and instances in Running state are terminated"
+echo -e "Wait is completed and instances in Running state are terminated"
 
-sleep 30
+sleep 15
 
-echo -e "\e[31m\e[1mDeleting auto-scaling-group"
+echo -e "\e[1mDeleting auto-scaling-group \e[0m"
 aws autoscaling delete-auto-scaling-group --auto-scaling-group-name webserverdemo
-echo -e "\e[91mAutoScaling group-name deleted"
+echo -e "AutoScaling group-name deleted"
 
-echo -e "\e[32m\e[1mDeleting launch-configuration-group"
+echo -e "\e[1mDeleting launch-configuration-group \e[0m"
 aws autoscaling delete-launch-configuration --launch-configuration-name webserver
-echo -e "\e[92mLaunch configuration deleted"
+echo -e "Launch configuration deleted"
 
-echo -e "\e[34m\e[1mDeleting load-balancer"
+echo -e "\e[1mDeleting load-balancer \e[0m"
 aws elb delete-load-balancer --load-balancer-name itmo-544
-echo -e "\e[94mLoad-balancer deleted"
+echo -e "Load-balancer deleted"
